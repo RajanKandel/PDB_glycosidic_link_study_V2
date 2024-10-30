f52196900918f5aa5f61d848c7df21c330cc67f2
Rajan <rajankandel406@gmail.com>
2024-10-30 09:39:20 -0400

    new projects created : a1_2D, a1_3D, a1_6D

---
+++ a1_3D/DGlcpa1-3DGlcpb/monosaccharide_bfmp_run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+pdbpath='ring_file.pdb'
+original_pdbpath='../../pdbs//6k0v.pdb'
+sugar_ring_ResID='1.G'
+bfmp_path='/home/rajan/apps/BFMP/detect_shape'
+
+# if [[ $# -ne 2 ]]; then
+#   echo "Error !!!"
+#   echo "USAGE: $0 <pdbpath> <sugar_ring_ResID>"
+#   exit 1
+# fi
+
+echo "$original_pdbpath $sugar_ring_ResID" >>monosaccharide_ring_conformations.txt
+echo "$original_pdbpath $sugar_ring_ResID" >>monosaccharide_ring_conformations.log
+
+sed -i "9s/.*/$sugar_ring_ResID/" pdb_input.txt
+$bfmp_path $pdbpath pdb_input.txt
+
+cat ring_conformations.txt >>monosaccharide_ring_conformations.txt
+cat ring_conformations.txt >>monosaccharide_ring_conformations.log
